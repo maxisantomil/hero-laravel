13caa223201d00801c31c601109ceb9fde97c499
Maxi <maxisantomil90@gmail.com>
2021-03-23 06:14:47 -0300

    Se modifica boton borrar , y se agrega modal de bootstrap

--- resources/views/admin/heroes/index.blade.php
+++ resources/views/admin/heroes/index.blade.php
@@ -45,14 +45,36 @@
                     <form action="{{route('admin.heroes.destroy',['id'=>$hero->id])}}" method="POST">
                       @csrf
                       @method('DELETE')
-                      <button type="submit" class= "btn btn-danger" >Borrar</button>
+                      <button type="button" class= "btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">Borrar</button>
+
+                             <!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Alerta</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <p>Esta seguro que desea borrar el heroe seleccionado?</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
+        @method('DELETE')
+        <button type="submit" class="btn btn-primary">Si Borrar</button>
+      </div>
+    </div>
+  </div>
+</div>
                     </form>
                   </div>
               </div>
             </td>
           </tr>
         @endforeach
-      
+
+ 
+
       
     </tbody>
   </table>
